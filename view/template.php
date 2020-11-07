df9c844daaeee7365a8bcb8010e33c40d25b0218
Thomas <thomasbuckley92@live.com>
2020-11-07 19:39:54 +0900

    changed name of my CSS file to landing.css

--- view/template.php
+++ view/template.php
@@ -8,7 +8,7 @@ $style = NULL;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="./public/css/landing.css">
 
     <!-- FONT LINKS -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,900;1,400&display=swap" rel="stylesheet">
