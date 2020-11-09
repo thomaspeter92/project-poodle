a01221dfe77d75e12825f4fb9a6850ccbab3e9f6
tiffany <tiffanyle717@gmail.com>
2020-11-09 17:39:01 +0900

    Contact Page 2

--- view/contactPageView.php
+++ view/contactPageView.php
@@ -2,20 +2,43 @@
 <?php ob_start(); ?>
 <div class="contactPage1">
     <h1>Get in touch</h1>
-    <img src="./projectPoodle/public/images/contactPageHeader.jpg">
+    <div class="topContent">
+        <div class="contactPageEmail">
+            <div class="circle"><h2><i class="far fa-envelope fa-3x"></i></h2></div>
+            <div class="email">
+                <h2>EMAIL</h2>
+                <p>contactus@contactus.com</p>
+            </div>
+        </div>
+        <div class="contactPageNumber">
+            <div class="circle"> <h2><i class="fas fa-phone fa-3x"></i></h2> </div>
+            <div class="phoneNumber">
+                <h2>PHONE</h2>
+                <p>+82 010 1234 5678</p>
+            </div>
+        </div>
+        <div class="contactPageFax">
+            <div class="circle"> <h2><i class="fas fa-fax fa-3x"></i></h2> </div>
+            <div class="faxNumber">
+                <h2>FAX</h2>
+                <p>+82 010 1234 5678</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="contactMiddle">
+    <div class="contactMiddleText">
+        <p>LET US KNOW</p>
+        <h1>WHAT YOU THINK</h1>
+        <p class="contactDivider"></p>
+    </div>
+    
+   
 </div>
 
-<div class="contactPageEmail">
-    <p>Email : contactus@contactus.com</p>
-</div>
-
-<div class="contactPageNumbers">
-    <p>Phone Number: +82 010 1234 5678</p>
-    <p>Fax Number +82 010 1234 5678</p>
-</div>
 
 <div class="contactPageHeadquarters">
-    <h1 style="text-align: center">Headquarters</h1>
+    <!-- <h1>HEADQUARTERS : </h1> -->
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.038753844758!2d126.96982341491137!3d37.530583733944795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca4068869c845%3A0xd50d67ce84380ab!2swcoding!5e0!3m2!1sen!2skr!4v1604890590704!5m2!1sen!2skr" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 </div>
 
