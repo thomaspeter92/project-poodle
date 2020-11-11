2b46ff0ec55a74df91eeed0c56644cdf5b89696d
Hong Shik Kim <hong@hongs.dev>
2020-11-11 12:08:40 +0900

    Fix an error after merging

--- controller/controller.php
+++ controller/controller.php
@@ -23,42 +23,6 @@ function showPetPreview($ownerId){
     require("./view/previewPet.php");
 }
 
-
-
-
-function login(){
-    require('./view/loginView.php');
-}
-
-function checkLogin ($params) {
-    $loginManager = new MemberManager();
-    $status = $loginManager->checkLogin($params);
-    if($status) {
-        header("Location: index.php");
-    }else {
-        // header("Location: index.php?action=login&error=login");
-
-    }
-}
-
-function registration (){
-    require('./view/registrationView.php');
-}
-
-function addNewMember($params){
-    $registrationManager = new MemberManager();
-    $register = $registrationManager->addNewMember($params);
-
-    echo $_SESSION["name"];
-
-    if($register){
-        $registrationManager->createSession($params);
-        header("Location: index.php?action=petPreview");
-    }else{ 
-        //header("Location: index.php?action=registration&error=registration"); //@TODO not going here on error
-    }
-
-}
 function aboutUs(){
     require('./view/aboutUsView.php');
 }
