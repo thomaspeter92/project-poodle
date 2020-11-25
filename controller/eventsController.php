c5bb6d887b4d9573a78153d3141fbc20264cfdb3
Hong Shik Kim <hong@hongs.dev>
2020-11-25 14:45:31 +0900

    Fix a bug about events list with no results

--- controller/eventsController.php
+++ controller/eventsController.php
@@ -16,12 +16,8 @@ function getGuestCountOfEvent($eventId) {
 }
 function showUpcomingEventsList($sessionID) {
     $manager = new EventManager();
-    $events = $manager->getUpcomingEvents();
-    if ($events) {
-        require('./view/eventsListView.php');
-    } else {
-        throw new Exception("Failed to show upcoming events!!", 2000);
-    }
+    $events = $manager->getUpcomingEvents();    
+    require('./view/eventsListView.php');
 }
 
 function showEventDetail($params) {
