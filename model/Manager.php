ea01e239737e7c14c7b944f2ff1535ad9cf1f683
Hong Shik Kim <hong@hongs.dev>
2021-03-25 05:28:22 +0900

    feat: Add a template to connect with RDS in AWS

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,17 @@ class Manager
 {
     protected function dbConnect()
     {
-        return new PDO('mysql:host=localhost;dbname=projectPoodle;charset=utf8', 'root', '');
+        $dbhost = "[DB_HOST]";
+        $dbport = "3306";
+        $dbname = "[DB_NAME]";
+        $charset = "utf8";
+
+        $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+        $username = "[USERNAME]";
+        $password = "[PASSWORD]";
+    
+        return new PDO($dsn, $username, $password);
+        // return new PDO('mysql:host=localhost;dbname=projectPoodle;charset=utf8', 'root', '');
     }
 }
 
