52cec8d51f7051bec0ae9040046d73e433a66ea1
Hong Shik Kim <hong@hongs.dev>
2021-03-27 07:39:19 +0900

    fix: Fix to enable to connect with local DB by the flag

--- model/Manager.php
+++ model/Manager.php
@@ -3,17 +3,22 @@ class Manager
 {
     protected function dbConnect()
     {
+        $CONNECT_TO_LOCALDB = true;
+
+        if ($CONNECT_TO_LOCALDB) {
+            return new PDO('mysql:host=localhost;dbname=projectPoodle;charset=utf8', 'root', ''); 
+        }
+
         $dbhost = "[DB_HOST]";
         $dbport = "3306";
         $dbname = "[DB_NAME]";
-        $charset = "utf8";
+        $charset = "utf8mb4";
 
         $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
         $username = "[USERNAME]";
         $password = "[PASSWORD]";
     
         return new PDO($dsn, $username, $password);
-        // return new PDO('mysql:host=localhost;dbname=projectPoodle;charset=utf8', 'root', '');
     }
 }
 
